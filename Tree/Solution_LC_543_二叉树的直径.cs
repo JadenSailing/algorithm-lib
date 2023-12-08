dc90886f242a5f863d0268ff307f644d595b9c98
jadensailing@126.com <jadensailing@126.com>
2023-12-08 22:46:25 +0800

    Create Solution_LC_543_二叉树的直径.cs

---
+++ Tree/Solution_LC_543_二叉树的直径.cs
@@ -0,0 +1,35 @@
+
+using LeetCode;
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution_LC_543_二叉树的直径
+{
+    private int ans = 0;
+    public int DiameterOfBinaryTree(TreeNode root)
+    {
+        DFS(root);
+        return ans;
+    }
+
+    //树形dp 返回子树的最大深度
+    public int DFS(TreeNode node)
+    {
+        if (node == null) return 0;
+        int left = 0, right = 0;
+        if (node.left != null) left = DFS(node.left);
+        if (node.right != null) right = DFS(node.right);
+        ans = Math.Max(ans, left + right);
+        return Math.Max(left, right) + 1;
+    }
+}
