e28c7e175cc211b3783dbe7023663171c42ca2c9
jadensailing@126.com <jadensailing@126.com>
2023-12-20 21:19:10 +0800

    Create Solution_LC_215_数组中的第K个最大元素.cs

---
+++ Heap/Solution_LC_215_数组中的第K个最大元素.cs
@@ -0,0 +1,64 @@
+﻿/*
+给定整数数组 nums 和整数 k，请返回数组中第 k 个最大的元素。
+
+请注意，你需要找的是数组排序后的第 k 个最大的元素，而不是第 k 个不同的元素。
+
+你必须设计并实现时间复杂度为 O(n) 的算法解决此问题。
+*/
+
+public class Solution_LC_215_数组中的第K个最大元素
+{
+    public int FindKthLargest(int[] nums, int k)
+    {
+        Heap heap = new Heap();
+        foreach (int v in nums) heap.Push(v);
+        int ans = 0;
+        while (k-- > 0) ans = heap.Pop();
+        return ans;
+    }
+}
+public class Heap
+{
+    private bool mIsMinHeap = false;
+    private List<int> list = new List<int>() { 0 };
+    private int size = 0;
+    public int Pop()
+    {
+        int res = list[1];
+        list[1] = list[size];
+        size--;
+        SiftDown(1);
+        return res;
+    }
+    public void Push(int x)
+    {
+        if (size == list.Count - 1)
+        {
+            list.Add(x);
+            size++;
+        }
+        else list[size] = x;
+        SiftUp(size);
+    }
+    private void SiftUp(int i)
+    {
+        while (i / 2 > 0)
+        {
+            int p = i / 2;
+            if (list[i] <= list[p]) break;
+            (list[i], list[p]) = (list[p], list[i]);
+            i /= 2;
+        }
+    }
+    private void SiftDown(int i)
+    {
+        while (i * 2 <= size)
+        {
+            int c = i * 2;
+            if (c + 1 <= size && list[c + 1] > list[c]) c++;
+            if (list[i] >= list[c]) break;
+            (list[i], list[c]) = (list[c], list[i]);
+            i = c;
+        }
+    }
+}
